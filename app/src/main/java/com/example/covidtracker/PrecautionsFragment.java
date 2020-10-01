655d40fa941a2dfab2953592545da3cd91d611fb
Dinesh B S <dineshbs444@gmail.com>
2020-10-02 02:46:58 +0530

    Added new style and added some colors to colors.xml

--- app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
+++ app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
@@ -8,8 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CountryFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PrecautionsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PrecautionsFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -17,12 +24,21 @@ public class CountryFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CountryFragment() {
+    public PrecautionsFragment() {
         // Required empty public constructor
     }
 
-    public static CountryFragment newInstance(String param1, String param2) {
-        CountryFragment fragment = new CountryFragment();
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PrecautionsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PrecautionsFragment newInstance(String param1, String param2) {
+        PrecautionsFragment fragment = new PrecautionsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -43,6 +59,6 @@ public class CountryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country, container, false);
+        return inflater.inflate(R.layout.fragment_precautions, container, false);
     }
 }
