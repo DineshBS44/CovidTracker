a77cb7b54d026646c38d3391f5c85c35f4f6038f
Dinesh B S <dineshbs444@gmail.com>
2020-10-05 00:44:52 +0530

    Added UI for precautions Fragment and also added list item for Recycler View!

--- app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
+++ app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
@@ -59,7 +59,7 @@ public class PrecautionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        getActivity().setTitle("Precautions");
+        getActivity().setTitle("Precaution");
         return inflater.inflate(R.layout.fragment_precautions, container, false);
     }
 }
