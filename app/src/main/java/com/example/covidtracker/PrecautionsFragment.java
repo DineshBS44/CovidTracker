a3c64463825679523b3316f28af7803a98eda492
Dinesh B S <dineshbs444@gmail.com>
2020-10-05 01:07:50 +0530

    Added Code to set Adapter in Precautions Fragment!

--- app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
+++ app/src/main/java/com/example/covidtracker/PrecautionsFragment.java
@@ -2,57 +2,35 @@ package com.example.covidtracker;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PrecautionsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.covidtracker.adapter.PrecautionsAdapter;
+import com.example.covidtracker.model.Precautions;
+
+import java.util.ArrayList;
+
+
 public class PrecautionsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView precautionsRecyclerView;
+    ArrayList<Precautions> precautions = new ArrayList<>();
 
     public PrecautionsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PrecautionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static PrecautionsFragment newInstance(String param1, String param2) {
         PrecautionsFragment fragment = new PrecautionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,5 +39,19 @@ public class PrecautionsFragment extends Fragment {
         // Inflate the layout for this fragment
         getActivity().setTitle("Precaution");
         return inflater.inflate(R.layout.fragment_precautions, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        precautions.add(new Precautions("Wash Hands","It is always a good precaution to Wash hands regularly at home",R.drawable.ic_wash_your_hands));
+
+        precautionsRecyclerView=view.findViewById(R.id.precautions_rv);
+        LinearLayoutManager precautionsLinearLayoutManager = new LinearLayoutManager(getContext());
+        precautionsLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        precautionsRecyclerView.setLayoutManager(precautionsLinearLayoutManager);
+        precautionsRecyclerView.setAdapter(new PrecautionsAdapter(precautions));
+
     }
 }
