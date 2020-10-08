9a3528068385d33e4608849a44ecbc6f6788c4b9
Dinesh B S <dineshbs444@gmail.com>
2020-10-08 12:49:36 +0530

    Added Shimmer effect for CountryDetail Activity!

--- app/src/main/java/com/example/covidtracker/CountryDetail.java
+++ app/src/main/java/com/example/covidtracker/CountryDetail.java
@@ -1,16 +1,25 @@
 package com.example.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.ethanhua.skeleton.Skeleton;
+import com.ethanhua.skeleton.SkeletonScreen;
+import com.example.covidtracker.adapter.PrecautionsAdapter;
 import com.example.covidtracker.model.Country;
 
 public class CountryDetail extends AppCompatActivity {
 
     TextView tvDetailCountryName, tvDetailTotalCases, tvDetailTodayCases, tvDetailTotalDeaths,
             tvDetailTodayDeaths, tvDetailTotalRecovered, tvDetailTotalActive, tvDetailTotalCritical;
+    private SkeletonScreen skeletonScreen1,skeletonScreen2,skeletonScreen3,skeletonScreen4;
+    private LinearLayout linearLayoutCountryDetail;
+    private LinearLayout linearLayout1,linearLayout2,linearLayout3,linearLayout4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +37,66 @@ public class CountryDetail extends AppCompatActivity {
         tvDetailTotalActive = findViewById(R.id.detail_total_active);
         tvDetailTotalCritical = findViewById(R.id.detail_total_critical);
 
+        linearLayoutCountryDetail=findViewById(R.id.country_detail_linear_layout);
+        linearLayout1=findViewById(R.id.linear_layout_1);
+        linearLayout2=findViewById(R.id.linear_layout_2);
+        linearLayout3=findViewById(R.id.linear_layout_3);
+        linearLayout4=findViewById(R.id.linear_layout_4);
 
         // call Country model class
-        Country covidCountry = getIntent().getParcelableExtra("EXTRA_COVID");
-
-        // set text view
+        final Country covidCountry = getIntent().getParcelableExtra("EXTRA_COVID");
         tvDetailCountryName.setText(covidCountry.getmCovidCountry());
-        tvDetailTotalCases.setText(Integer.toString(covidCountry.getmCases()));
-        tvDetailTodayCases.setText(covidCountry.getmTodayCases());
-        tvDetailTotalDeaths.setText(covidCountry.getmDeaths());
-        tvDetailTodayDeaths.setText(covidCountry.getmTodayDeaths());
-        tvDetailTotalRecovered.setText(covidCountry.getmRecovered());
-        tvDetailTotalActive.setText(covidCountry.getmActive());
-        tvDetailTotalCritical.setText(covidCountry.getmCritical());
+
+        skeletonScreen1 = Skeleton.bind(linearLayout1)
+                .shimmer(true)
+                .angle(20)
+                .duration(1200)
+                .load(R.layout.skeleton_list_item_1)
+                .show();
+        skeletonScreen2 = Skeleton.bind(linearLayout2)
+                .shimmer(true)
+                .angle(20)
+                .duration(1200)
+                .load(R.layout.skeleton_list_item_1)
+                .show();
+        skeletonScreen3 = Skeleton.bind(linearLayout3)
+                .shimmer(true)
+                .angle(20)
+                .duration(1200)
+                .load(R.layout.skeleton_list_item_2)
+                .show();
+        skeletonScreen4 = Skeleton.bind(linearLayout4)
+                .shimmer(true)
+                .angle(20)
+                .duration(1200)
+                .load(R.layout.skeleton_list_item_1)
+                .show();
+
+
+        Runnable mRunnable;
+        Handler mHandler = new Handler();
+
+        mRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+
+                skeletonScreen1.hide();
+                skeletonScreen2.hide();
+                skeletonScreen3.hide();
+                skeletonScreen4.hide();
+                // set text view
+                tvDetailTotalCases.setText(Integer.toString(covidCountry.getmCases()));
+                tvDetailTodayCases.setText(covidCountry.getmTodayCases());
+                tvDetailTotalDeaths.setText(covidCountry.getmDeaths());
+                tvDetailTodayDeaths.setText(covidCountry.getmTodayDeaths());
+                tvDetailTotalRecovered.setText(covidCountry.getmRecovered());
+                tvDetailTotalActive.setText(covidCountry.getmActive());
+                tvDetailTotalCritical.setText(covidCountry.getmCritical());
+            }
+        };
+
+        mHandler.postDelayed(mRunnable, 1300);
 
     }
 }
