4191b64dfd1b07953eb9e6f8609976d78be3c2be
Dinesh B S <dineshbs444@gmail.com>
2020-10-08 13:40:42 +0530

    Added Splash Screen with Lottie Animation and changes the app icon!

--- app/src/main/java/com/example/covidtracker/activities/MainActivity.java
+++ app/src/main/java/com/example/covidtracker/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.covidtracker;
+package com.example.covidtracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentContainerView;
@@ -8,6 +8,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.covidtracker.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
